c5a0f5476fbc09186b354b96ac376688a70d924b
fshangala <fshangala@gmail.com>
2022-06-02 09:44:36 +0200

    work flow established

--- dclean.py
+++ dclean.py
@@ -34,14 +34,12 @@ class MainWindow(QMainWindow):
        geometry = self.screen().availableGeometry()
        self.resize(geometry.width() * 0.8, geometry.height() * 0.7)
 
-       # Menu
+       # Menus
        self.file_menu = self.menuBar().addMenu("File")
+       self.setFileMenuActions()
 
-       # Exit QAction
-       exit_action = QAction("Exit", self)
-       exit_action.setShortcut(QKeySequence.Quit)
-       exit_action.triggered.connect(self.close)
-       self.file_menu.addAction(exit_action)
+       self.home_menu = self.menuBar().addMenu("Home")
+       self.setHomeMenuActions()
 
        # Stack widget as central widget
        stack = QStackedWidget()
@@ -54,16 +52,37 @@ class MainWindow(QMainWindow):
        # Load the plugins
        self.loadPlugins()
     
+    # Menu actions
+    def setFileMenuActions(self):
+       # Exit QAction
+       exit_action = QAction("Exit", self)
+       exit_action.setShortcut(QKeySequence.Quit)
+       exit_action.triggered.connect(self.close)
+       self.file_menu.addAction(exit_action)
+    
+    def setHomeMenuActions(self):
+       # Dashboard QAction
+       dashboard_action = QAction("Dashboard", self)
+       dashboard_action.triggered.connect(lambda: self.centralWidget().setCurrentIndex(0))
+       self.home_menu.addAction(dashboard_action)
+    
     def loadPlugins(self):
         for plugin in config.PLUGINS:
-            a = plugin.main()
+            a:QWidget = plugin.main()
             self.centralWidget().addWidget(a)
+            a.index = self.centralWidget().indexOf(a)
 
             if callable(getattr(a,'setMenus',None)):
-                a.setMenus()
+                a.setMenus(self.menuBar())
             
             if callable(getattr(a,'setHomePageUI',None)):
-                a.setHomePageUI()
+                a.setHomePageUI(self.homePage.layout())
+            
+            if callable(getattr(a,'setFileMenuActions',None)):
+                a.setFileMenuActions(self.file_menu)
+            
+            if callable(getattr(a,'setHomeMenuActions',None)):
+                a.setHomeMenuActions(self.home_menu)
 
 if __name__ == "__main__":
 
--- plugins/settings/main.py
+++ plugins/settings/main.py
@@ -1,23 +1,42 @@
 from PySide6.QtWidgets import (
     QWidget,
     QVBoxLayout,
-    QLabel
+    QLabel,
+    QMenuBar,
+    QGridLayout,
+)
+from PySide6.QtGui import (
+    QAction,
+    QKeySequence
 )
 
 class SettingsPage(QWidget):
     def __init__(self,*args,**kwargs):
         QWidget.__init__(self)
 
+        self.index = None
+
         # Layout
-        self.pageLayout = QVBoxLayout()
-        self.setLayout(self.pageLayout)
+        pageLayout = QVBoxLayout()
+        self.setLayout(pageLayout)
 
         # Initialize the layout
         self.setUI()
     
-    def setMenus(self):
-        file_menu = self.parent().parent().menuBar().addMenu("Settings")
+    def setMenus(self,menuBar: QMenuBar):
+        settings_menu = menuBar.addMenu("Settings")
+
+        general_action = QAction("General", self)
+        general_action.triggered.connect(self.on_general_action)
+        settings_menu.addAction(general_action)
+    
+    def on_general_action(self):
+        self.parent().setCurrentIndex(self.index)
     
     def setUI(self):
+        words = QLabel(text="Hello Settings")
+        self.layout().addWidget(words)
+    
+    def setHomePageUI(self,layout: QGridLayout):
         words = QLabel(text="Hello World")
-        self.pageLayout.addWidget(words)
+        layout.addWidget(words)
